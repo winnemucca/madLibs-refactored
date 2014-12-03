3f5eb64441c9937af9e2e40a0668e1a57ca96512
Steven Kauyedauty <skauyedauty@gmail.com>
2014-12-03 16:13:09 -0700

    added simple animations

--- main.js
+++ main.js
@@ -1,31 +1,17 @@
-var app = angular.module('myApp',[]).
+var app = angular.module('myApp',['ngAnimate']).
 controller('NamingController', function($scope) {
 	// the empty bracket allows me to add values.  a hard coded string will cause errors
 	$scope.customName =[];
 
-	// $scope.update = function (customName) {
-	// 	$scope.customName = angular.copy(customName);
-	// };
-
-	// $scope.reset = function () {
-	// 	angular.copy($scope.customName);
-
-	// };
-	// $scope.reset();
+	
 
 	var original = $scope.customName;
 	$scope.reset = function () {
 		$scope.myForm.$setPristine()
 		$scope.user = angular.copy(original);
+		$scope.customName ="";
 
 	};
-
-	// $scope.reset = function() {
-	// $scope.myForm.$setPristine()
-	// $scope.user = angular.copy($scope.customName);
-	// // $scope.myForm.$setPristine()
-	// };
-
+	
 });
 
-// scope.myForm.$setPristine()
